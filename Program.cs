aadfb8e6ac4e1cc97f8b62433ea6554eac482e2c
Jo├úo Paulo <joaopaulo102003@gmail.com>
2025-04-14 10:01:50 -0300

    tarefa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseDefaultFiles(); 
+app.UseStaticFiles(); 
+
+app.MapGet("/sobre", async context =>
+{
+    await context.Response.SendFileAsync("wwwroot/sobre.html");
+});
+
+app.Run();
